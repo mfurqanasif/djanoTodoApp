dd8ad1f566b86e467c98977a8b62dd93a965ef6a
Ubuntu <ubuntu@ip-172-31-45-182.eu-north-1.compute.internal>
2023-12-15 11:09:44 +0000

    my first push

---
+++ autoPush.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "-Pushing data on GIT repo"
+echo "-creainh a folder"
+mkdir django
+echo "-moving into the created dir"
+cd django
+echo "-Creating a pyhton file"
+touch myPyFile.py
+echo "print('i will work hard to complete this assignment')" > myPyFile.py
+
