0c04409f419cf0bd72db898911f78b48db228c6e
Ubuntu <ubuntu@ip-172-31-45-182.eu-north-1.compute.internal>
2023-12-15 12:15:23 +0000

    Auotmatic push

--- autoDeployPush.sh
+++ autoDeployPush.sh
@@ -14,6 +14,6 @@ git add .
 git commit -m "Auotmatic push"
 git push origin main
 echo USERNAME , TOKEN
-git push https://${USERNAME}:${TOKEN}@github.com/mfurqanasif/djanoTodoApp.git origin main
+git push "https://${USERNAME}:${TOKEN}@github.com/${USERNAME}/djanoTodoApp.git" origin main
 
 
