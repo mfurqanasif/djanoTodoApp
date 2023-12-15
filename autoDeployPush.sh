d3727168d7e88a6d27f945dd58f72cb000fe7819
Ubuntu <ubuntu@ip-172-31-45-182.eu-north-1.compute.internal>
2023-12-15 11:35:01 +0000

    Auotmatic push

--- autoDeployPush.sh
+++ autoDeployPush.sh
@@ -1,12 +1,13 @@
 #!/bin/bash 
 
-mkdir autoPushFolder
+mkdir autoPushFolder2
 cd autoPushFolder
-touch myPythonFile.py
+touch myPythonFile2.py
 echo "print('writing into python file ')" > myPythonFile.py
 
 cd ..
 
 git add .
 git commit -m "Auotmatic push"
+git push origin main
 
